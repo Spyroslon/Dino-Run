3acf8aea03d6eb2c05b56be2e13fdd8e24a78460
Spyros Lontos <spyros.londos@gmail.com>
2025-01-05 23:25:31 +0100

    adding notes for future fixes

--- train.py
+++ train.py
@@ -4,7 +4,7 @@ from stable_baselines3.common.callbacks import CheckpointCallback
 from dino_env import DinoEnv
 
 # Create the environment
-env = make_vec_env(DinoEnv, n_envs=1)
+env = make_vec_env(DinoEnv, n_envs=1) # consider increasing this
 
 # Define a checkpoint callback to save models every 10,000 steps
 checkpoint_callback = CheckpointCallback(
@@ -19,10 +19,15 @@ model = PPO(
     env,
     verbose=1,
     learning_rate=1e-4,
-    n_steps=4096,
-    batch_size=128,
+    n_steps=4096, 
+    batch_size=128, # see how much memory this uses and consider increasing it
     tensorboard_log="./tensorboard_logs/",
 )
+
+# Add distance to tensorboard
+# Sometimes when the page is out of focus, the Status keeps says "JUMPING" and the distance increases
+# As soon as I focus the page, the Dino just crashes. I think there is a bug.
+# Should I keep it as picture in picture?
 
 # Train the model with the checkpoint callback
 model.learn(total_timesteps=100000, callback=checkpoint_callback)
